bdc65dffe441cc0e92eaaa1f74d016a56ba1680e
Karim Mouline <113158763+k-mouline@users.noreply.github.com>
2024-10-10 17:43:40 -0400

    Update AlienMover.java

--- AlienMover.java
+++ AlienMover.java
@@ -9,16 +9,16 @@ import javafx.scene.layout.Pane;
 public class AlienMover {
     private Alien alien;
     public AlienMover(Pane alienPane, HBox buttonPane) {
-        setupButtons(buttonPane);
+        this.setupButtons(buttonPane);
         this.alien = new Alien(alienPane);
     }
 
     public void setupButtons(HBox buttonPane) {
         Button b1 = new Button("Move Left!");
         Button b2 = new Button("Move Right!");
-        buttonPane.getChildren().addAll(b1, b2);
         buttonPane.setSpacing(Constants.BUTTON_SPACING);
         buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.getChildren().addAll(b1, b2);
         b1.setOnAction((ActionEvent e) -> this.alien.moveLeft());
         b2.setOnAction((ActionEvent e) -> this.alien.moveRight());
     }
